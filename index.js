414461636b164a29630c79016468d101991ad522
Idan Stark <idanstark42@gmail.com>
2019-02-06 22:40:00 +0200

    matches block

--- index.js
+++ index.js
@@ -6,8 +6,12 @@ const fs = Promise.promisifyAll(require('fs'))
 const { scheduele } = require('./lib/schedueler')
 const { writeCSV } = require('./lib/csv_formatting')
 
+console.log('Init...')
 fs.readFileAsync(path.resolve(__dirname, '.', 'dev', 'input.json'), 'utf8').then(fileContent => {
 	const input = JSON.parse(fileContent)
+	console.log('Starting...')
 	const output = scheduele(input)
-	writeCSV(output, path.resolve(__dirname, '.', 'dev', 'output.csv'))
+	writeCSV(output, path.resolve(__dirname, '.', 'dev', 'output.csv')).then(() => {
+		console.log('Done.')
+	})
 })
